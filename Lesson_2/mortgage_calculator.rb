4910c1b1733a0289dec5f7738e4e9d22c8e7d256
kutikina <git@tmac.com.au>
2016-02-07 18:27:24 +1100

    refactor mortgage calculator

--- Lesson_2/mortgage_calculator.rb
+++ Lesson_2/mortgage_calculator.rb
@@ -1,36 +1,59 @@
-def exp(c, n)
-  r = (1 + c)
-  e = r**n
-end
 
 def prompt(message)
   puts "=> #{message}"
 end
 
-def monthly_rate(rate)
-  rate/100/12 
+amount = ''
+
+prompt "Welcome to the Mortgage Calculator.  "
+
+loop do
+  loop do
+    prompt "What is the loan amount in dollars"
+    amount = gets.chomp.to_i
+    if amount > 0
+      break
+    else
+      prompt "You must enter an amount in dollars that is more than 0"
+    end
+  end
+
+  rate = ''
+  monthly_rate = ''
+
+  loop do
+    prompt "What is the Annual Percentage Rate? (e.g. 5 for 5%)"
+    rate = gets.chomp.to_f
+    if rate > 0
+      monthly_rate = rate / 100 / 12
+      break
+    else
+      prompt "You must enter a rate that is a positive number"
+    end
+  end
+
+  duration = ''
+
+  loop do
+    prompt "What is the loan duration in years?"
+    duration = gets.chomp.to_i
+    if duration > 0
+      duration = duration * 12
+      break
+    else  
+      prompt "You must enter a valid duration greater than 0 years"
+    end
+  end
+
+  payment = amount * (monthly_rate * ((monthly_rate + 1) ** duration) / (((monthly_rate + 1) ** duration) - 1))
+
+  puts "Your loan deails are:"
+  puts "Loan amount: $ #{amount}."
+  puts "Loan period: #{duration / 12} years."
+  puts "Annual Percentage Rate: #{rate.to_i}%"
+  puts "Based on these details, the fixed monthly payment is $#{payment.round(2)}."
+
+  prompt "Would you like to run it again? y for Yes"
+  answer = gets.downcase.chomp
+  break unless answer.start_with?("y")
 end
-
-def duration_in_months(duration)
-  duration * 12
-end
-
-prompt("What is the loan amount in dollars")
-amount = gets.chomp.to_i
-
-prompt ("What is the Annual Percentage Rate? (e.g. 5 for 5%)")
-rate = gets.chomp.to_f
-
-prompt("What is the loan duration in years?")
-duration = gets.chomp.to_i
-
-numerator = amount * (monthly_rate(rate) * exp(monthly_rate(rate), duration_in_months(duration)))
-denominator = exp(monthly_rate(rate), duration_in_months(duration)) - 1
-payment = numerator / denominator
-
-
-puts "Your loan deails are:"
-puts "Loan amount: $ #{amount}."
-puts "Loan period: #{duration} years."
-puts "Annual Percentage Rate: #{rate}%"
-puts "Based on these details, the fixed monthly payment is $#{payment.round(2)}."
